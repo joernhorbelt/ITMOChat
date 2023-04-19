6b77fdc399c0d5be289380bbd8c63f71848b30fe
boem <joernhorbelt@gmx.net>
2023-04-19 14:55:50 +0200

    changed eventhandler

---
+++ src/server/ServerThread.java
@@ -0,0 +1,45 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerThread extends Thread {
+
+    private Socket socket;
+    private ArrayList<ServerThread> threadList;
+    private PrintWriter output;
+
+    public ServerThread(Socket socket, ArrayList<ServerThread> threads) {
+        this.socket = socket;
+        this.threadList = threads;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(socket.getOutputStream(), true);
+
+            while (true) {
+                String outputString = input.readLine();
+                if (outputString.equals("exit")) {
+                    break;
+                }
+                printToAllClients(outputString);
+                System.out.println("Server recieved " + outputString);
+            }
+        } catch (Exception e) {
+            System.out.println("Error occured in main of server"+ e.getStackTrace());
+        }
+    }
+
+    private void printToAllClients(String outputString) {
+        for (ServerThread serverThread : threadList) {
+            serverThread.output.println(outputString);
+        }
+    }
+
+}
